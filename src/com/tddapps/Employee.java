5c3cee092f3ac97a63f062b29e6a8e1a4588743b
Camilo Sanchez <camilin87@gmail.com>
2018-05-02 21:34:21 -0400

    create a first example that works

--- src/com/tddapps/Employee.java
+++ src/com/tddapps/Employee.java
@@ -1,9 +1,27 @@
 package com.tddapps;
 
+import javax.persistence.*;
+
+/*
+create table EMPLOYEE (
+   id serial,
+   first_name VARCHAR(20) default NULL,
+   last_name  VARCHAR(20) default NULL,
+   salary     INT  default NULL,
+   PRIMARY KEY (id)
+);
+ */
+@Entity
+@Table(name = "EMPLOYEE")
 public class Employee {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
+    @Column(name = "first_name")
     private String firstName;
+    @Column(name = "last_name")
     private String lastName;
+    @Column(name = "salary")
     private int salary;
 
     public Employee() {}
@@ -13,35 +31,15 @@ public class Employee {
         this.salary = salary;
     }
 
-    public int getId() {
-        return id;
-    }
+    public int getId() { return id; }
+    public void setId( int id ) { this.id = id; }
 
-    public void setId( int id ) {
-        this.id = id;
-    }
+    public String getFirstName() { return firstName; }
+    public void setFirstName( String first_name ) { this.firstName = first_name; }
 
-    public String getFirstName() {
-        return firstName;
-    }
+    public String getLastName() { return lastName; }
+    public void setLastName( String last_name ) { this.lastName = last_name; }
 
-    public void setFirstName( String first_name ) {
-        this.firstName = first_name;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName( String last_name ) {
-        this.lastName = last_name;
-    }
-
-    public int getSalary() {
-        return salary;
-    }
-
-    public void setSalary( int salary ) {
-        this.salary = salary;
-    }
+    public int getSalary() { return salary; }
+    public void setSalary( int salary ) { this.salary = salary; }
 }
--- src/com/tddapps/Main.java
+++ src/com/tddapps/Main.java
@@ -1,7 +1,31 @@
 package com.tddapps;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println("Starting program");
+        System.out.println(System.getProperty("user.dir"));
+
+        SessionFactory factory = null;
+
+        try {
+            factory = new Configuration().configure().buildSessionFactory();
+        } catch (Throwable ex) {
+            System.err.println("Failed to create sessionFactory object." + ex);
+            throw new ExceptionInInitializerError(ex);
+        }
+
+        var repository = new EmployeeRepository(factory);
+
+        var empID1 = repository.addEmployee("Zara", "Ali", 1000);
+        var empID2 = repository.addEmployee("Daisy", "Das", 5000);
+        repository.addEmployee("John", "Paul", 10000);
+
+
+        repository.listEmployees();
+        repository.updateEmployee(empID1, 5000);
+        repository.deleteEmployee(empID2);
+        repository.listEmployees();
     }
 }
